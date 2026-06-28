2b8efee11547539dc99531110a3a9f042b13b324
midhunmathew2016-afk <midhunmathew2016@gmail.com>
2026-06-28 20:38:50 +0530

    Add build output directory to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG', '**/*.JPG', '**/*.JPEG']
+  assetsInclude: ['**/*.PNG', '**/*.JPG', '**/*.JPEG'],
+  build: {
+    outDir: 'docs'
+  }
 })
